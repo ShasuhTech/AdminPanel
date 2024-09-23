4958a3fb21abd6a8093bcdafe19aa0fe9457a89d
faizahmad729 <61235978+faizahmad729@users.noreply.github.com>
2024-09-24 00:43:08 +0530

    next config update

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
     return config;
   },
   reactStrictMode: true,
-  transpilePackages: ['@mui/x-charts','@mui/x-date-pickers'],
+  // transpilePackages: ['@mui/x-charts','@mui/x-date-pickers'],
   
   images: {
     domains: ['salon-documents.s3.ap-south-1.amazonaws.com'],
