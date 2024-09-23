2d2fee8a51fdf7839f21548a4c35abfd9b1f169c
faizahmad729 <61235978+faizahmad729@users.noreply.github.com>
2024-09-24 02:55:47 +0530

    some changes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const path = require('path')
 
 module.exports = {
-  webpack: (config, { isServer }) => {
-    // Modify config if needed
-    return config;
-  },
+ 
   reactStrictMode: true,
-  // transpilePackages: ['@mui/x-charts','@mui/x-date-pickers'],
-  
+  transpilePackages: ['@mui/x-charts','@mui/x-date-pickers','@mui/x-date-pickers/AdapterDayjs','@mui/icons-material'],
   images: {
-    domains: ['salon-documents.s3.ap-south-1.amazonaws.com'],
+    domains: ['salon-documents.s3.ap-south-1.amazonaws.com','salon-documents-prod.s3.ap-south-1.amazonaws.com',
+  ],
   }, 
 }
